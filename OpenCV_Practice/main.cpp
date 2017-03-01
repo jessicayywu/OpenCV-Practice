6da193ffaaf43d95423d247e0a592b4b966fb921
jessicayywu <jessicayywu@yahoo.com.tw>
2017-03-01 17:57:43 +0800

    Add Camera
    Picture-In-Picture

--- OpenCV_Practice/main.cpp
+++ OpenCV_Practice/main.cpp
@@ -5,23 +5,34 @@ using namespace cv;
 using namespace std;
 
 int main() {
-	Mat frame;
-	VideoCapture cap("input/jaguar.mp4");
+	Mat video_frame, camera_frame, dst_frame;
+	VideoCapture video("input/jaguar.mp4");
+	VideoCapture camera(0);
 
-	if (!cap.isOpened()) {
+	if (!video.isOpened() || !camera.isOpened()) {
 		cout << "Cannot open the video" << endl;
 		return -1;
 	}
 
+	namedWindow("OpenCV Practice", 1);
+
 	while (1) {
-		cap.read(frame);
+		video >> video_frame;
+		camera >> camera_frame;
 
-		if (frame.rows > 0)
-			imshow("Video Frame", frame);
-		else
-			break;
+		resize(camera_frame, camera_frame, Size(camera_frame.cols / 3, camera_frame.rows / 3));
+		
+		Mat dst_frame = video_frame(Rect(0, 0, camera_frame.cols, camera_frame.rows));
+		
+		addWeighted(dst_frame, 0, camera_frame, 1, 0, dst_frame);
 
-		waitKey(30);
+		//if (video_frame.rows > 0)
+		imshow("OpenCV Practice", video_frame);
+		//else
+		//	break;
+
+
+		if(waitKey(30)>=0) break;
 	}
 	return 0;
 }
