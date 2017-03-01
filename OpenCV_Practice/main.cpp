ab51ba2ce554160a1232072d98dbf4c16f819108
jessicayywu <jessicayywu@yahoo.com.tw>
2017-03-01 19:51:42 +0800

    Add Edge Detection
    1. Add edge detection
    2. Rename some variables

--- OpenCV_Practice/main.cpp
+++ OpenCV_Practice/main.cpp
@@ -5,7 +5,8 @@ using namespace cv;
 using namespace std;
 
 int main() {
-	Mat video_frame, camera_frame, dst_frame;
+	Mat video_frame, camera_frame, edge_frame;
+	Mat gray_camera, edges, ROI;
 	VideoCapture video("input/jaguar.mp4");
 	VideoCapture camera(0);
 
@@ -21,10 +22,18 @@ int main() {
 		camera >> camera_frame;
 
 		resize(camera_frame, camera_frame, Size(camera_frame.cols / 3, camera_frame.rows / 3));
-		
-		Mat dst_frame = video_frame(Rect(0, 0, camera_frame.cols, camera_frame.rows));
-		
-		addWeighted(dst_frame, 0, camera_frame, 1, 0, dst_frame);
+
+		cvtColor(camera_frame, gray_camera, CV_BGR2GRAY);
+		blur(gray_camera, edges, Size(3, 3));
+		Canny(gray_camera, edges, 100, 200);
+		edge_frame = Scalar::all(0);
+		gray_camera.copyTo(edge_frame, edges);
+		cvtColor(edge_frame, edge_frame, CV_GRAY2BGR);
+
+		addWeighted(camera_frame, 0.5, edge_frame, 0.5, 0, camera_frame);
+		ROI = video_frame(Rect(0, 0, camera_frame.cols, camera_frame.rows));
+
+		addWeighted(ROI, 0, camera_frame, 1, 0, ROI);
 
 		//if (video_frame.rows > 0)
 		imshow("OpenCV Practice", video_frame);
